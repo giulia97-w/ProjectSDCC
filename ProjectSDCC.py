ad7d35f16fff4c74a52c602317a08cd9afe6d3f8
giulia97-w <giulia.menichins@gmail.com>
2021-08-30 11:31:53 +0200

    Upload

--- ProjectSDCC.py
+++ ProjectSDCC.py
@@ -57,12 +57,14 @@ def date(expire):
 def createNewWindow():
     newWindow = tk.Toplevel(window)
     newWindow.resizable(width=0, height=0)
-    labelExample = tk.Label(newWindow, text = "COVID 19 VACCINE")
+    labelExample = tk.Label(newWindow, text = "COVID 19 VACCINE", font=('Helvetica bold',32))
     newWindow.geometry("1300x900")
     newWindow.grid_columnconfigure(0, weight=20)
     labelExample.grid()
-    buttonExample.grid()
-
+    buttonOperation.grid(row = 260)
+    buttonOperation.place(relx=0.81, rely=0.60)
+    
+    
     #opening the image
     background_image = Image.open("covidCell.jpg")
     #add image file
@@ -72,62 +74,14 @@ def createNewWindow():
     background_label.image = background_photo
     background_label.place(relwidth=1, relheight=1)
     
+    
     #label implements a display box where you can place text or images
     newWelcome_label = tk.Label(newWindow,
                                 text="COVID 19 VACCINE",
                                         font=("Helvetica bold", 32), bg='medium aquamarine', fg = 'black')
     newWelcome_label.grid(row=0, column=0, sticky="WE", padx=300, pady=10)
     
-    #label implements a display box where you can place text or images
-    ttk.Label(newWindow, text= 'Enter the batch of the vaccine you want to search for:', font=('Helvetica', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
-    #The Entry widget is used to accept single-line text strings from a user
-    text_input = tk.Entry(newWindow, font=('Helvetica', 14))
-    text_input.grid(row=2, column=0, sticky="WE", padx=300)
-    #Add buttons
-    download_button = tk.Button(newWindow, text="SEND", command=lambda : get_vax(), font = 'Helvetica', bg = 'medium aquamarine', relief = RAISED, width=1,
-                               height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
-    download_button.grid(row=7, column=0, sticky="WE", pady=10, padx=300)
     
-    #The purpose of this widget is to display text, an image, or both. Generally the content is static, but your program can change the text or the image, in this case is static
-    ttk.Label(newWindow, text= 'Enter the batch of the vaccine you want to delete:', font=('Helvetica', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
-    
-
-    #The Entry widget is used to accept single-line text strings from a user
-    second_text_input = tk.Entry(newWindow, font=('Helvetica', 14))
-    second_text_input.grid(row=20, column = 0, sticky = "WE", padx  =300)
-    #Add Buttons
-    second_button = tk.Button(newWindow, text="DELETE VACCINE", command= lambda: delete_vax(), font = 'Helvetica', bg = 'medium aquamarine', relief = RAISED, width=1,
-                               height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1 )
-    second_button.grid(row=26, column=0, pady=10, sticky="WE", padx = 300)
-    
-    #The purpose of this widget is to display text, an image, or both. Generally the content is static, but your program can change the text or the image, in this case is static
-    ttk.Label(newWindow, text= 'Enter the batch of the vaccine whose number of doses you want to change:', font=('Century Schoolbook bold', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
-    
-    
-    firth_input = tk.Entry(newWindow, font=('Century Schoolbook bold', 14))
-    firth_input.insert  (END, '')
-    firth_input.grid(row=110, column=0, sticky="WE", padx=300) 
-
-    
-    ttk.Label(newWindow, text= 'Enter the new number of doses of the vaccine:', font=('Century Schoolbook bold', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
-    
-    
-    firth_inputC = tk.Entry(newWindow, font=('Century Schoolbook bold', 14))
-    firth_inputC.insert  (END, '')
-    firth_inputC.grid(row=120, column=0, sticky="WE", padx=300) 
-    
-        
-    firth_button = tk.Button(newWindow, text="UPDATE THE DEADLINE", command=lambda : update_vax(), font = 'Helvetica', bg = 'medium aquamarine', relief = RAISED,width=1,
-                               height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
-    firth_button.grid(row=400, column=0, sticky="WE", pady=10, padx=300)
-    
-    
-    exit_button = tk.Button(newWindow, text = "EXIT", command=lambda : close(), font='Helvetica', relief = RAISED, activebackground='lightblue3', cursor = 'hand2', bg = 'white', bd = 1)
-    exit_button.grid( sticky='NE', row = 450, padx = 60)
-    exit_button.place(relx=0.93, rely=0.01, relwidth=0.05, relheight=0.05)
-    
-
-
     #get_vax() function to get vaccine information by entering the lot
     def get_vax():
         if text_input.get():
@@ -140,10 +94,21 @@ def createNewWindow():
             response = "Enter the vaccine batch"
             
         #Text widgets provide advanced capabilities that allow you to edit a multiline text and format the way it has to be displayed, such as changing its color and font.
-        textwidget = tk.Text(newWindow, height=5)
+        textwidget = tk.Text(newWindow, height=4)
         textwidget.insert(tk.END, text_parsing(response))
         textwidget.grid(row=4, column=0, sticky="WE", padx=300, ipady = 10)
        
+    #label implements a display box where you can place text or images
+    ttk.Label(newWindow, text= 'Enter the batch of the vaccine you want to search for:', font=('Helvetica', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
+    
+    #The Entry widget is used to accept single-line text strings from a user
+    text_input = tk.Entry(newWindow, font=('Helvetica', 10))
+    text_input.grid(row=2, column=0, sticky="WE", padx=300)
+    
+    #SEND BUTTON
+    download_button = tk.Button(newWindow, text="SEND", command=get_vax, font = ('Helvetica',12), bg = 'medium aquamarine', relief = RAISED, width=1,
+                               height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
+    download_button.grid(row=7, column=0, sticky="WE", pady=10, padx=300)
     
     #delete_vax() function to delete a vaccine record by entering the batch  
     def delete_vax():
@@ -160,9 +125,23 @@ def createNewWindow():
         textwidget = tk.Text(newWindow, height=2)
         textwidget.insert(tk.END, response)
         textwidget.grid(row=23, column=0, sticky="WE", padx=300, pady=10)
-
-
     
+    #The purpose of this widget is to display text, an image, or both. Generally the content is static, but your program can change the text or the image, in this case is static
+    ttk.Label(newWindow, text= 'Enter the batch of the vaccine you want to delete:', font=('Helvetica', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
+    
+
+    #The Entry widget is used to accept single-line text strings from a user
+    #Batch input for the delete function
+    second_text_input = tk.Entry(newWindow, font=('Helvetica', 10))
+    second_text_input.grid(row=20, column = 0, sticky = "WE", padx  =300)
+    
+    #DELETE BUTTON
+    second_button = tk.Button(newWindow, text="DELETE VACCINE", command=delete_vax, font = ('Helvetica',12), bg = 'medium aquamarine', relief = RAISED, width=1,
+                               height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1 )
+    second_button.grid(row=26, column=0, pady=10, sticky="WE", padx = 300)
+    
+    
+       
     #update_vax() function to update the information on the expiration of a vaccine in the table
     def update_vax():
         if firth_input.get():
@@ -186,8 +165,32 @@ def createNewWindow():
         textwidget.grid(row=175, column=0, sticky="WE", padx=300, pady=10)
     
     
+    #The purpose of this widget is to display text, an image, or both. Generally the content is static, but your program can change the text or the image, in this case is static
+    ttk.Label(newWindow, text= 'Enter the batch of the vaccine whose number of doses you want to change:', font=('Century Schoolbook bold', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
     
-        
+    #batch input for the update function
+    firth_input = tk.Entry(newWindow, font=('Century Schoolbook bold', 10))
+    firth_input.grid(row=110, column=0, sticky="WE", padx=300) 
+
+    
+    ttk.Label(newWindow, text= 'Enter the new number of doses of the vaccine:', font=('Century Schoolbook bold', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE")
+    
+    #number of doses for vaccine input for the update function
+    firth_inputC = tk.Entry(newWindow, font=('Century Schoolbook bold', 10))
+    firth_inputC.grid(row=120, column=0, sticky="WE", padx=300) 
+    
+    #UPDATE BUTTON   
+    firth_button = tk.Button(newWindow, text="UPDATE THE DEADLINE", command=lambda : update_vax(), font = ('Helvetica',12), bg = 'medium aquamarine', relief = RAISED,width=1,
+                               height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
+    firth_button.grid(row=400, column=0, sticky="WE", pady=10, padx=300)
+    
+    #EXIT BUTTON
+    exit_button = tk.Button(newWindow, text = "EXIT", command=lambda : close(), font=('Helvetica',12), relief = RAISED, activebackground='lightblue3', cursor = 'hand2', bg = 'white', bd = 1)
+    exit_button.grid( sticky='NE', row = 450, padx = 60)
+    exit_button.place(relx=0.93, rely=0.01, relwidth=0.05, relheight=0.05)
+    
+       
+            
 #scan_vax() function to scan the dynamoDB "VaxTables" table
 def scan_vax():
 
@@ -197,15 +200,15 @@ def scan_vax():
     text_response = json.dumps(response, indent=6)
     
     
-    
-
-    
     textwidget = tk.Text(height = 7)
     textwidget.insert(tk.END, text_response)
     textwidget.grid(row=50, column=0, sticky="WE", padx=300, pady=10)
     
-                
-    
+#Add buttons in the master window
+#GET DATABASE INFORMATION BUTTON
+third_button = tk.Button(text="GET DATABASE INFORMATION", command=scan_vax, font = ('Helvetica', 12), bg = 'medium aquamarine', relief = RAISED, width=1,
+                           height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
+third_button.grid(row=41, column=0, pady=10, sticky="WE", padx = 300)    
 
 
 
@@ -238,6 +241,50 @@ def insert_vax():
     textwidget.grid(row=200, column=0, sticky="WE", padx=300, pady=10)
 
     
+
+#The purpose of this widget is to display text, an image, or both. Generally the content is static, but your program can change the text or the image, in this case is static
+ttk.Label(window, text= 'Enter batch:', font=('Century Schoolbook bold', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE", row = 80)
+
+
+#The Entry widget is used to accept single-line text strings from a user
+#batch input for the insert function
+fifth_input = tk.Entry(font=('', 10))
+fifth_input.grid(row=90, column=0, sticky="WE", padx=300) 
+
+
+ttk.Label(window, text= 'Enter the name of the Vaccine:', font=('Century Schoolbook bold', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE", row = 91)
+
+#name input for the insert function
+fifth_inputB = tk.Entry(font=('', 10))
+fifth_inputB.grid(row=92, column=0, sticky="WE", padx=300) 
+
+
+ttk.Label(window, text= 'Enter pharmaceutical company:', font=('Century Schoolbook bold', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE", row = 93)
+
+#pharmaceutical company input for the insert function
+fifth_inputC = tk.Entry(font=('Century Schoolbook bold', 10))
+fifth_inputC.grid(row=94, column=0, sticky="WE", padx=300) 
+
+
+ttk.Label(window, text= 'Enter number of doses per vaccine', font=('Century Schoolbook bold', 10), background = 'medium aquamarine').grid(padx = 300, sticky="WE",row = 95 )
+
+
+#number of doses input for the insert function
+fifth_inputD = tk.Entry(font=('Century Schoolbook bold', 10))
+fifth_inputD.grid(row=96, column=0, sticky="WE", padx=300) 
+
+
+ttk.Label(window, text= 'Enter expiration:', font=('Century Schoolbook bold', 10), background = 'medium aquamarine' ).grid(padx = 300, sticky="WE", row = 97)
+
+
+#exiparion input for the insert function
+fifth_inputE = tk.Entry(font=('Century Schoolbook bold', 10))
+fifth_inputE.grid(row=98, column=0, sticky="WE", padx=300) 
+
+#ADD BUTTON
+fifth_button = tk.Button(text="ADD VACCINE", command=insert_vax, font = ('Helvetica',12), bg = 'medium aquamarine', relief = RAISED, width=1,
+                           height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
+fifth_button.grid(row=250, column=0, sticky="WE", pady=5, padx=300)
     
 
 
@@ -272,69 +319,18 @@ welcome_label.grid(row=0, column=0, sticky="WE", padx=300, pady=10)
 
 
 
-
-
-
-#Add buttons in the master window
-third_button = tk.Button(text="GET DATABASE INFORMATION", command= lambda: scan_vax(), font = 'Helvetica', bg = 'medium aquamarine', relief = RAISED, width=1,
-                           height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
-third_button.grid(row=41, column=0, pady=10, sticky="WE", padx = 300)
-
-
-#The purpose of this widget is to display text, an image, or both. Generally the content is static, but your program can change the text or the image, in this case is static
-ttk.Label(window, text= 'Enter batch:', font=('Century Schoolbook bold', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE", row = 80)
-
-
-#The Entry widget is used to accept single-line text strings from a user
-fifth_input = tk.Entry(font=('', 14))
-fifth_input.insert(END, '')
-fifth_input.grid(row=90, column=0, sticky="WE", padx=300) 
-
-
-ttk.Label(window, text= 'Enter the name of the Vaccine:', font=('Century Schoolbook bold', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE", row = 91)
-
-fifth_inputB = tk.Entry(font=('', 14))
-fifth_inputB.insert(END, '')
-fifth_inputB.grid(row=92, column=0, sticky="WE", padx=300) 
-
-
-ttk.Label(window, text= 'Enter pharmaceutical company:', font=('Century Schoolbook bold', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE", row = 93)
-
-fifth_inputC = tk.Entry(font=('Century Schoolbook bold', 14))
-fifth_inputC.insert  (END, '')
-fifth_inputC.grid(row=94, column=0, sticky="WE", padx=300) 
-
-
-ttk.Label(window, text= 'Enter number of doses per vaccine', font=('Century Schoolbook bold', 14), background = 'medium aquamarine').grid(padx = 300, sticky="WE",row = 95 )
-
-
-
-fifth_inputD = tk.Entry(font=('Century Schoolbook bold', 14))
-fifth_inputD.insert  (END, '')
-fifth_inputD.grid(row=96, column=0, sticky="WE", padx=300) 
-
-
-ttk.Label(window, text= 'Enter expiration:', font=('Century Schoolbook bold', 14), background = 'medium aquamarine' ).grid(padx = 300, sticky="WE", row = 97)
-
-
-
-fifth_inputE = tk.Entry(font=('Century Schoolbook bold', 14))
-fifth_inputE.insert  (END, '')
-fifth_inputE.grid(row=98, column=0, sticky="WE", padx=300) 
-fifth_button = tk.Button(text="ADD VACCINE", command=lambda : insert_vax(), font = 'Helvetica', bg = 'medium aquamarine', relief = RAISED, width=1,
-                           height=1, activebackground='lightblue3', cursor = 'hand2', bd = 1)
-fifth_button.grid(row=250, column=0, sticky="WE", pady=5, padx=300)
-#Adding exit button
-exit_button = tk.Button(window, text = "EXIT", command=lambda : close(), bd=1, font='Helvetica', relief = RAISED, activebackground='lightblue3', cursor = 'hand2', bg = 'white')
+#EXIT BUTTON
+exit_button = tk.Button(window, text = "EXIT", command=lambda : close(), bd=1, font=('Helvetica',12), relief = RAISED, activebackground='lightblue3', cursor = 'hand2', bg = 'white')
 exit_button.grid( sticky='NE', padx= 60)
-exit_button.place(relx=0.93, rely=0.01, relwidth=0.05, relheight=0.05)
+exit_button.place(relx=0.90, rely=0.01, relwidth=0.05, relheight=0.05)
 
-#Adding "OTHER OPERATION' button in the master window
-buttonExample = tk.Button(window, 
+
+#OTHER OPERATIONS BUTTON
+buttonOperation = tk.Button(window, 
               text="OTHER OPERATIONS",
-              command=createNewWindow, bd=1, font='Helvetica', relief = RAISED, bg = 'medium aquamarine', activebackground='lightblue3', cursor = 'hand2')
-buttonExample.grid(row=270,  sticky='N')
-buttonExample.place(relx=0.81, rely=0.67)
+              command=createNewWindow, bd=1, font=('Helvetica',12), relief = RAISED, bg = 'medium aquamarine', activebackground='lightblue3', cursor = 'hand2')
+buttonOperation.grid(row=260,  sticky='N')
+buttonOperation.place(relx=0.81, rely=0.60)
 
 
 
